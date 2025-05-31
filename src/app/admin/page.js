e7dcd1427b705a262557e62c4babdfe354aa2642
Mauro Nicolas Sahid <mnico.amaya38@gmail.com>
2025-05-31 12:05:17 -0300

    v1.0.2

--- src/app/admin/page.js
+++ src/app/admin/page.js
@@ -4,7 +4,7 @@ export default function AdminPage() {
     <div className="admin">
       <h1>Panel de Administración</h1>
       <p>Gestión de productos, órdenes, y más.</p>
-      {/* Puedes incluir formularios y simulaciones de edición para agregar valor */}
+      {}
     </div>
   );
 }
